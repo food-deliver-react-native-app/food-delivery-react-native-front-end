957e5afe16eb03f3323c8bd28a925ec638860658
Beqael <beqaelashvili3@gmail.com>
2025-08-07 18:43:05 +0400

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-6xl hover:scale-110   transition duration-300  ease-in-out hover:cursor-pointer  font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
